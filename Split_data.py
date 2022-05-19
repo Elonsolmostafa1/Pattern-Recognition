3e5513e6ab04517f438865cfe7bc0dde8c309075
Elonsolmostafa1 <mostafaahmed21121997@gmail.com>
2022-05-19 15:11:21 +0200

    splitting data

---
+++ Split_data.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import splitfolders
+
+male_data = "Dataset\\Males\\"
+female_data = "Dataset\\Females\\"
+
+splitfolders.ratio(male_data,"D:\\Third Year 2nd\\Projects\\NN Project\\Pattern-Recognition\\Data_split",seed=1337,ratio=(0.8,0.0,0.2))
+splitfolders.ratio(female_data,"D:\\Third Year 2nd\\Projects\\NN Project\\Pattern-Recognition\\Data_split",seed=1337,ratio=(0.8,0.0,0.2))
