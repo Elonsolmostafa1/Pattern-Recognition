72b462cd53487981d677a211bb5ff6e8962c1412
makrion <miklaziz3112000@gmail.com>
2022-05-30 01:02:33 +0200

    pipeline-reconstruction + svm + performance analysis

--- Split_data.py
+++ Split_data.py
@@ -7,8 +7,8 @@ This is a temporary script file.
 
 import splitfolders
 
-male_data = "Dataset\\Males\\"
-female_data = "Dataset\\Females\\"
+male_data = "Dataset/Males/"
+female_data = "Dataset/Females/"
 
 splitfolders.ratio(male_data,"Data_split",seed=1337,ratio=(0.8,0.0,0.2))
 splitfolders.ratio(female_data,"Data_split",seed=1337,ratio=(0.8,0.0,0.2))
